dadd569f7bf41ae671d9b763ef0c31e9a310bd37
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-03-10 16:55:21 +0100

    init django app "shop" added django.contrib.gis and shop to installed_apps

--- nearbyshops/nearbyshops/settings.py
+++ nearbyshops/nearbyshops/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'shops'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'nearbyshops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gis',
+        'USER': 'user001',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
